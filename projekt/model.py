e3e28e6f2dcc547ae816c68fc0698daac2a6806c
Lukasz Czeropski <lukaszczeropski@gmail.com>
2022-01-12 15:50:38 +0100

    experiment-script, added command line params

--- projekt/model.py
+++ projekt/model.py
@@ -1,4 +1,5 @@
-
+#%%
+import argparse
 import joblib
 import numpy as np
 import pandas as pd
@@ -10,17 +11,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+#%%
+parser = argparse.ArgumentParser(description='Parser')
+parser.add_argument('train', type=str, nargs=1,
+                    help='train data path' )
+parser.add_argument('--target', type=int, nargs=1,default=0,
+                    help='target id ' )
+parser.add_argument('--split', type=float, nargs=1,default=0.8,
+                    help='split ratio float ' )
+parser.add_argument('--model', type=int, nargs=1,default=0,
+                    help='1 to use different model' )
+parser.add_argument('--pre', type=str, nargs=1,default=None,
+                    help='to predict data path' )
 
+args = parser.parse_args()
+print(vars(args))
+args = vars(args)
 
-def read_data():
-    data = pd.read_json(input()) #/Users/lczeropski/Documents/usersessions/dataset.json
+def read_data(path):
+    data = pd.read_json(path) #/Users/lczeropski/Documents/usersessions/dataset.json
     return data
-print('write train file path')
-train = read_data()
-print('write target id')
-target = int(input())
-def data_prep(data,target = target):
-    tgt = data[data['user_id'] == target]
+train = read_data((args['train'][0]))
+target = args['target']
+
+
+def data_prep(data,tgt=pd.DataFrame(None)):
     sites = pd.json_normalize(data.sites) 
     dt=data.drop('sites',axis = 1) 
     for i in range(0,len(sites.columns)):
@@ -32,28 +48,31 @@ def data_prep(data,target = target):
     data["time"] = pd.to_timedelta(data["time"])
     data["time"] = data["time"].dt.components["hours"]*60 + data["time"].dt.components["minutes"] 
     data['date'] = pd.to_datetime(data['date']).view(np.int64) 
-    data["gender"].replace({'m':0,'f':1},inplace = True)    
+    data["gender"].replace({'m':0,'f':1},inplace = True)
+    if tgt.shape[0]==0: 
+        tgt = data[data['user_id']==target]
+    else :
+        tgt = tgt   
     for i in ['browser','os','locale','location']:
         tem = tgt[i].unique()
         for j in (data[i].unique()):
             if j not in tem:
                 data[i].replace(to_replace = j,value = 'Other',inplace = True)
-    tgt = data[data['user_id']==target]
     for i in range(0,len(sites.columns)):
         tem = tgt['site'+str(i)].value_counts()[:10].index
         for j in set(data["site"+str(i)]):
             if j not in tem:
                 data["site"+str(i)].replace(to_replace=j,value='Other',inplace=True)
-    y = data["user_id"]
-    y = y.apply(lambda x: -1 if x==target else x)
-    y = y.apply(lambda x: 0 if x>=0  else 1)
-    X = data.drop("user_id",axis = 1)
-    return X,y,target
+
+    return data,tgt
+
+train,tgt = data_prep(train)
+y = train["user_id"]
+y = y.apply(lambda x: 0 if x==target else 1)
+X = train.drop("user_id",axis = 1)
 
 
-X,y,target = data_prep(train)
-
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,random_state=10,stratify=y)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=args['split'], test_size=1-args['split'],random_state=10,stratify=y)
 
 
 cat_cols = [cname for cname in X_train.columns if  
@@ -69,7 +88,8 @@ preprocessor = ColumnTransformer(
         ('num', numerical_transformer, numerical_cols),
         ('hot', categorical_transformer_oh, cat_cols)
     ])
-model = XGBClassifier(objective='binary:logistic',
+if args['model']==0:
+    model = XGBClassifier(objective='binary:logistic',
                       n_estimators = 150,
                       eval_metric = 'auc',
                       max_depth = 3,
@@ -77,6 +97,8 @@ model = XGBClassifier(objective='binary:logistic',
                       min_child_weight = 0.5,
                       scale_pos_weight = 1,
                       use_label_encoder=False)
+else:
+    model = RandomForestClassifier(max_depth=3, class_weight = 'balanced',n_estimators = 150)
 pipe = Pipeline(steps=[('preprocessor', preprocessor),
                        ('standardscaler', StandardScaler(with_mean=False)),
                               ('model', model)
@@ -104,7 +126,10 @@ tn, fp, fn, tp = confusion_matrix(y_valid,preds).ravel()
 print('True negatives',tn,'\nFalse positives',fp,'\nFalse negatives',fn,'\nTrue positives',tp)
 
 joblib.dump(pipe,'find_user_model')
-print('write data path to make prediction')
-to_pred= read_data()
-test = data_prep(to_pred)
-print(predict(to_pred))
+if args['pre']:
+    print('making prediction on data')
+    to_pred= read_data((args['pre'][0]))
+    test = data_prep(to_pred,tgt)[0]
+    predictions = predict(test)
+    print(predictions)
+
