8507115f4b4ccdcef5acdcca1bcee6ae365d2a6c
Lukasz Czeropski <lukaszczeropski@gmail.com>
2022-01-13 14:10:44 +0100

    Created class for data preparation

--- projekt/model.py
+++ projekt/model.py
@@ -15,59 +15,66 @@ from xgboost import XGBClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 
 parser = argparse.ArgumentParser(description='Parser')
-parser.add_argument('train', type=pathlib.Path,
-                    help='train data path' )
+parser.add_argument('dataset_path', type=pathlib.Path,
+                    help='dataset path to train model' )
 parser.add_argument('--target', type=int,default=0,
-                    help='target id ' )
+                    help='target id' )
 parser.add_argument('--split', type=float,default=0.8,
                     help='split ratio float ' )
-parser.add_argument('--model', type=int,default=0,
-                    help='1 to use different model' )
-parser.add_argument('--pre', type=pathlib.Path,default=None,
-                    help='to predict data path' )
+parser.add_argument('--model', type=str,default='xgboost',
+                    help='supported models are xgboost and gradient_boost' )
+parser.add_argument('--pre_path', type=pathlib.Path,default=None,
+                    help='dataset path to make predictions' )
 
 args = parser.parse_args()
 print(vars(args))
 args = vars(args)
+class Data_prep:
+    global target
+    target = args['target']
+    def __init__(self,path):
+        self.path = path
+        self.data = pd.read_json(self.path) 
+    def _flatten(self):
+        sites = pd.json_normalize(self.data.sites)
+        self.lensi = len(sites.columns)
+        dt=self.data.drop('sites',axis = 1) 
+        for i in range(0,len(sites.columns)):
+            dt['site'+str(i)] = pd.json_normalize(sites[i]).site 
+            dt['site_len'+str(i)] = pd.json_normalize(sites[i]).length 
+            dt['site'+str(i)].fillna('None',inplace=True) 
+            dt['site_len'+str(i)].fillna(0,inplace = True) 
+        self.data = dt 
+        self.data["time"] = pd.to_timedelta(self.data["time"])
+        self.data["time"] = self.data["time"].dt.components["hours"]*60 + self.data["time"].dt.components["minutes"] 
+        self.data['date'] = pd.to_datetime(self.data['date']).view(np.int64) 
+        self.data["gender"].replace({'m':0,'f':1},inplace = True)
+    def _separate_tgt(self,tar=target):
+        global separeted 
+        if 'user_id' in self.data.columns:
+            separeted = self.data[self.data['user_id']==tar]
+               
+    def _reduce(self):   
+        for i in ['browser','os','locale','location']:
+                tem = separeted[i].unique()
+                for j in (self.data[i].unique()):
+                    if j not in tem:
+                        self.data[i].replace(to_replace = j,value = 'Other',inplace = True)
+        for i in range(0,self.lensi):
+                tem = separeted['site'+str(i)].value_counts()[:10].index
+                for j in set(self.data["site"+str(i)]):
+                    if j not in tem:
+                        self.data["site"+str(i)].replace(to_replace=j,value='Other',inplace=True)
+    def procces(self):
+        self._flatten()
+        self._separate_tgt()
+        self._reduce()
+        return self.data  
+    def __repr__(self):
+        return repr(self.data)
 
-def read_data(path):
-    data = pd.read_json(path) 
-    return data
-train = read_data(args['train'])
-target = args['target']
+train=Data_prep(args['dataset_path']).procces()
 
-
-def data_prep(data,tgt=pd.DataFrame(None)):
-    sites = pd.json_normalize(data.sites) 
-    dt=data.drop('sites',axis = 1) 
-    for i in range(0,len(sites.columns)):
-        dt['site'+str(i)] = pd.json_normalize(sites[i]).site 
-        dt['site_len'+str(i)] = pd.json_normalize(sites[i]).length 
-        dt['site'+str(i)].fillna('None',inplace=True) 
-        dt['site_len'+str(i)].fillna(0,inplace = True) 
-    data = dt 
-    data["time"] = pd.to_timedelta(data["time"])
-    data["time"] = data["time"].dt.components["hours"]*60 + data["time"].dt.components["minutes"] 
-    data['date'] = pd.to_datetime(data['date']).view(np.int64) 
-    data["gender"].replace({'m':0,'f':1},inplace = True)
-    if tgt.shape[0]==0: 
-        tgt = data[data['user_id']==target]
-    else :
-        tgt = tgt   
-    for i in ['browser','os','locale','location']:
-        tem = tgt[i].unique()
-        for j in (data[i].unique()):
-            if j not in tem:
-                data[i].replace(to_replace = j,value = 'Other',inplace = True)
-    for i in range(0,len(sites.columns)):
-        tem = tgt['site'+str(i)].value_counts()[:10].index
-        for j in set(data["site"+str(i)]):
-            if j not in tem:
-                data["site"+str(i)].replace(to_replace=j,value='Other',inplace=True)
-
-    return data,tgt
-
-train,tgt = data_prep(train)
 y = train["user_id"]
 y = y.apply(lambda x: 0 if x==target else 1)
 X = train.drop("user_id",axis = 1)
@@ -89,7 +96,7 @@ preprocessor = ColumnTransformer(
         ('num', numerical_transformer, numerical_cols),
         ('hot', categorical_transformer_oh, cat_cols)
     ])
-if args['model']==0:
+if args['model']=='xgboost':
     model = XGBClassifier(objective='binary:logistic',
                       n_estimators = 150,
                       eval_metric = 'auc',
@@ -98,7 +105,7 @@ if args['model']==0:
                       min_child_weight = 0.5,
                       scale_pos_weight = 1,
                       use_label_encoder=False)
-else:
+elif args['model']=='gradient_boost':
     model = GradientBoostingClassifier(n_estimators=300, learning_rate=0.01,
     max_depth=6)
 pipe = Pipeline(steps=[('preprocessor', preprocessor),
@@ -128,10 +135,9 @@ tn, fp, fn, tp = confusion_matrix(y_valid,preds).ravel()
 print('True negatives',tn,'\nFalse positives',fp,'\nFalse negatives',fn,'\nTrue positives',tp)
 
 joblib.dump(pipe,'find_user_model')
-if args['pre']:
+if args['pre_path']:
     print('making prediction on data')
-    to_pred= read_data((args['pre'][0]))
-    test = data_prep(to_pred,tgt)[0]
-    predictions = predict(test)
+    to_pred=Data_prep(args['pre_path']).procces()
+    predictions = predict(to_pred)
     print(predictions)
 
