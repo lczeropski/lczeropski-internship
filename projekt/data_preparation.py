85aabb06f45d7e7f545e59681f2d2c54fa64c95f
Lukasz Czeropski <lukaszczeropski@gmail.com>
2022-01-20 15:43:00 +0100

    Added predict form loaded model, lintered the code

--- projekt/data_preparation.py
+++ projekt/data_preparation.py
@@ -1,79 +1,165 @@
-#%%
-import pandas as pd
-import numpy as np
+"""[summary]
+Python module to prepare data before fitting into model
+Supports command line arguments
+dataset_path, type:pathlib.Path dataset path to train model
+--target, type:int,default=0, target id
+--pre_path, type:pathlib.Path, default=None, dataset path to make predictions
+Returns:
+    Pandas DataFrame saved to csv file
+"""
 import argparse
 import pathlib
-class Data_prep:
+import pandas as pd
+import numpy as np
+class DataPrep:
+    """[summary]
+    Class object containing data engineering functions
+    Data is loaded form .json file
+    Transformed into flat table
+    Pattern is set to rows that contain given target
+    Rest of data is transformed using pattern
+    If pattern cannot be made it uses one made previosly to given target
+    Saves changed data and pattern to csv files
+    Returns:
+        Pandas DataFrame : Dataframe with changed values according to target pattern
+    """
     def __init__(self,path,target_id):
+        """[summary]
+        Object initialization
+        Args:
+            path (pathlib.Path): .json file path from with data will be loaded
+            target_id (Int): Integer value, id of user that pattern will be based of
+        """
         self.path = path
         self.data = pd.read_json(self.path)
         self.target = target_id
-    def _flatten(self):
-        sites = pd.json_normalize(self.data.sites)
-        self.lensi = len(sites.columns)
-        dt=self.data.drop('sites',axis = 1) 
-        for i in range(0,len(sites.columns)):
-            dt['site'+str(i)] = pd.json_normalize(sites[i]).site 
-            dt['site_len'+str(i)] = pd.json_normalize(sites[i]).length 
-            dt['site'+str(i)].fillna('None',inplace=True) 
-            dt['site_len'+str(i)].fillna(0,inplace = True) 
-        self.data = dt 
-        self.data["time"] = pd.to_timedelta(self.data["time"])
-        self.data["time"] = self.data["time"].dt.components["hours"]*60 + self.data["time"].dt.components["minutes"] 
-        self.data['date'] = pd.to_datetime(self.data['date']).view(np.int64) 
-        self.data["gender"].replace({'m':0,'f':1},inplace = True)
+        self.lensi = 0
+        self.pattern = None
+    def get_lensi(self):
+        """[summary]
+        Getter method of lensi value,
+        Max length of sites dictonary in .json file
+        Returns:
+            Int: Max number of sites
+        """
+        return self.lensi
+    def set_lensi(self,length):
+        """[summary]
+        Setter method of lensi value
+        Assing given lenght value to object attribute lensi
+        Args:
+            length (Int): Length on dictonary in .json file
+        """
+        self.lensi = length
     def get_target(self):
+        """[summary]
+        Getter method of target value
+        Return object target attribute
+        Returns:
+            Int: Id number
+        """
         return self.target
     def set_target(self,target_id):
+        """[summary]
+        Setter method target attribute
+        Args:
+            target_id (Int): Sets given number to attribute target
+        """
         self.target = target_id
     def get_pattern(self):
+        """[summary]
+        Getter method pattern attribute
+        Returns:
+            Pandas DataFrame: DataFrame only with rows that contains user_id as in target attribute
+        """
         return repr(self.pattern)
     def _generate_pattern(self):
+        """[summary]
+        Method
+        Chooses rows from data attribute that has only user_id as in target attribute.
+        Saves it into csv file
+        """
         separeted = self.data[self.data['user_id']==self.target]
-        separeted.to_csv('user'+'_'+str(self.target))
+        separeted.to_csv('user'+'_'+str(self.target)+".csv",index = False)
     def _set_pattern(self):
-        self.pattern = pd.read_csv('user'+'_'+str(self.target))            
-    def _reduce(self):      
+        """[summary]
+        Setter method
+        Loads csv file matching target attribute
+        """
+        self.pattern = pd.read_csv('user'+'_'+str(self.target)+".csv")
+    def _flatten(self):
+        """[summary]
+        Method
+        Flattens .json file
+        """
+        sites = pd.json_normalize(self.data.sites)
+        self.set_lensi(len(sites.columns))
+        temp_frame=self.data.drop('sites',axis = 1)
+        for i in range(0,len(sites.columns)):
+            temp_frame['site'+str(i)] = pd.json_normalize(sites[i]).site
+            temp_frame['site_len'+str(i)] = pd.json_normalize(sites[i]).length
+            temp_frame['site'+str(i)].fillna('None',inplace=True)
+            temp_frame['site_len'+str(i)].fillna(0,inplace = True)
+        self.data = temp_frame
+        self.data["time"] = pd.to_timedelta(self.data["time"])
+        self.data["time"] = (self.data["time"].dt.components["hours"]*60
+                            + self.data["time"].dt.components["minutes"])
+        self.data['date'] = pd.to_datetime(self.data['date']).view(np.int64)
+        self.data["gender"].replace({'m':0,'f':1},inplace = True)
+    def _reduce(self):
+        """[summary]
+        Method
+        Reduces cardinality of data using pattern attribute
+        """
         for i in ['browser','os','locale','location']:
-                tem = self.pattern[i].unique()
-                for j in (self.data[i].unique()):
-                    if j not in tem:
-                        self.data[i].replace(to_replace = j,value = 'Other',inplace = True)
+            tem = self.pattern[i].unique()
+            for j in self.data[i].unique():
+                if j not in tem:
+                    self.data[i].replace(to_replace = j,value = 'Other',inplace = True)
         for i in range(0,self.lensi):
-                tem = self.pattern['site'+str(i)].value_counts()[:10].index
-                for j in set(self.data["site"+str(i)]):
-                    if j not in tem:
-                        self.data["site"+str(i)].replace(to_replace=j,value='Other',inplace=True)
+            tem = self.pattern['site'+str(i)].value_counts()[:10].index
+            for j in set(self.data["site"+str(i)]):
+                if j not in tem:
+                    self.data["site"+str(i)].replace(to_replace=j,value='Other',inplace=True)
     def __repr__(self):
-        return repr(self.data)
+        """[summary]
+        Method
+        Returns object data as Pandas DataFrame
+        Returns:
+            Pandas DataFrame: Object data attribute
+        """
+        return repr(pd.DataFrame(self.data))
     def procces(self):
+        """[summary]
+        Has 3 or 4 steps
+        #1 Uses flatten method
+        ##2 If there is user_is column in flatten data it generates pattern, generate_pattern method
+        #3 Loads pattern to given target, set_pattern method
+        #4 Uses pattern to reduce cardinality, reduce method
+        Saves proccesed data into csv file if there was step 2 then it is train data file
+        """
         self._flatten()
         if "user_id" in self.data.columns:
             self._generate_pattern()
         self._set_pattern()
         self._reduce()
         if "user_id" in self.data.columns:
-            self.data.to_csv('train_user'+str(self.target))
+            self.data.to_csv('train_user'+str(self.target)+".csv",index = False)
         else:
-            self.data.to_csv('to_pred_user'+str(self.target))
-        
-# %%
+            self.data.to_csv('to_pred_user'+str(self.target)+".csv",index = False)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parser')
     parser.add_argument('dataset_path', type=pathlib.Path,
-                        help='dataset path to train model' )
+                        help='dataset path to train model')
     parser.add_argument('--target', type=int,default=0,
-                        help='target id' )
+                        help='target id')
     parser.add_argument('--pre_path', type=pathlib.Path,default=None,
-                        help='dataset path to make predictions' )
-    
-    args = parser.parse_args()
-    print(vars(args))
-    args = vars(args)
-    
-    train=Data_prep(args['dataset_path'],args['target'])
+                        help='dataset path to make predictions')
+    args = vars(parser.parse_args())
+    print(args)
+    train=DataPrep(args['dataset_path'],args['target'])
     train.procces()
     if args['pre_path']:
-        pre_data = Data_prep(args['pre_path'],train.get_target())
+        pre_data = DataPrep(args['pre_path'],train.get_target())
         pre_data.procces()
-# %%
+        
--- projekt/model.py
+++ projekt/model.py
@@ -1,4 +1,26 @@
-
+"""
+Module to train test model, improve it by predict threshold
+Model detects user with given id (default id=0 :Codename Joe)
+Model requires data transformed by data_preparation.py
+Supports command line arguments
+--target, type:int, default=0, target id'
+--split, type:float, default=0.8, split ratio
+--model, type:str, default='xgboost'
+    supported models are xgboost, gradient_boost, tree, forest, ada
+--predict, type:bool, default=False,
+    Boolean value, True to use model to make prediction on dataset'
+--dataset_path, type=pathlib.Path,
+    dataset path to train model, use when dataset is not in working directory'
+--dataset_pred, type=pathlib.Path,
+    dataset path to make predictions, used when dataset is not in working directory
+--load_model', type=pathlib.Path,
+    model file path to use it in predictions,
+    use when you want just prediction made by model that was already trained
+Returns:
+   predictions.csv: csv file with predictions, only if --predict is True
+   find_user_model.sav: trained model and threshold value to improve precision/recall ratio,
+    only if --load_model is None
+"""
 import argparse
 import pathlib
 import joblib
@@ -6,115 +28,133 @@ import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import (AdaBoostClassifier, GradientBoostingClassifier,
+                              RandomForestClassifier)
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from xgboost import XGBClassifier
-from sklearn.ensemble import GradientBoostingClassifier,RandomForestClassifier, AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+def cal_threshold(mod_to_improve, x_val, y_val):
+    """
+    Function to calculate the best threshold to improve
+    precision/recall ratio
+    Args:
+        model (sklearn model): Model to witch improvment will be find
+        x_valid (Pandas DataFrame): Validation data frame with
+        y_valid (array): An array with true values of users id used to validate the prediction
 
-
-def cal_threshold(model,X_val,y_val):
-    preds = model.predict_proba(X_val)[:,1]
-    precision, recall, thresholds = metrics.precision_recall_curve(y_val,preds)
+    Returns:
+        model (sklearn model): Model to witch optimal threshold was found
+        threshold (Float): Optimal threshold value
+    """
+    bad_preds = mod_to_improve.predict_proba(x_val)[:,1]
+    precision, recall, thresholds = metrics.precision_recall_curve(y_val, bad_preds)
     fscore = (2 * precision * recall) / (precision + recall)
-    ix = np.argmax(fscore)
-    threshold =thresholds[ix]
-    return model,threshold
-def th_predict(test,model,th):
-    preds = model.predict_proba(test)
-    preds = (preds[:,1] >= th).astype('int')
-    return preds
+    index = np.argmax(fscore)
+    threshold = thresholds[index]
+    return mod_to_improve, threshold
+def th_predict(test, model_improved, treshold=0.5):
+    """
+    Function to make better predictons using found threshold
+    Args:
+        test (Pandas DataFrame): Data to make prediction on
+        model_improved (sklearn model): model to witch threshold was found
+        treshold (Float): Threshold value
+    Returns:
+        preds (array): An array containing improved predictions
+    """
+    good_preds = model_improved.predict_proba(test)
+    good_preds = (good_preds[:,1] >= treshold).astype('int')
+    return good_preds
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parser')
     parser.add_argument('--target', type=int,default=0,
-                        help='target id' )
+            help='target id' )
     parser.add_argument('--split', type=float,default=0.8,
-                        help='split ratio float ' )
+            help='split ratio float ' )
     parser.add_argument('--model', type=str,default='xgboost',
-                        help='supported models are xgboost, gradient_boost, tree, forest, ada' )
+            help='supported models are xgboost, gradient_boost, tree, forest, ada')
     parser.add_argument('--predict', type=bool,default=False,
-                        help='Boolean value, True to use model to make prediction on dataset' )
+            help='Boolean value, True to use model to make prediction on dataset')
     parser.add_argument('--dataset_path', type=pathlib.Path,
-                        help='dataset path to train model, use when dataset is not in working directory' )
+            help='dataset path to train model, use when dataset is not in working directory')
     parser.add_argument('--dataset_pred', type=pathlib.Path,
-                        help='dataset path to make predictions, used when dataset is not in working directory' )
-
+            help='dataset path to make predictions, used when dataset is not in working directory')
+    parser.add_argument('--load_model', type=pathlib.Path,
+            help='model file path to use it in predictions')
     args = parser.parse_args()
     args = vars(args)
-
-    if args['dataset_path']:
-        train_path = args['dataset_path']
-    else:
-        train_path = 'train_user'+str(args['target'])
-    train = pd.read_csv(train_path)
-    y = train["user_id"]
-    y = y.apply(lambda x: 0 if x==args['target'] else 1)
-    X = train.drop("user_id",axis = 1)
-
-
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=args['split'], test_size=1-args['split'],random_state=10,stratify=y)
-
-
-    cat_cols = [cname for cname in X_train.columns if  
-                            X_train[cname].dtype == "object"] 
-    numerical_cols = [cname for cname in X_train.columns if X_train[cname].dtype in ['int64', 'float64']]
-
-    categorical_transformer_oh = Pipeline(steps=[ 
-        ('onehot', OneHotEncoder(handle_unknown='ignore',sparse=False))
-    ])
-    numerical_transformer = SimpleImputer(strategy='mean')
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('num', numerical_transformer, numerical_cols),
-            ('hot', categorical_transformer_oh, cat_cols)
+    print(args)
+    if not args['load_model']:
+        if args['dataset_path']:
+            TRAIN_PATH = args['dataset_path']
+        else:
+            TRAIN_PATH = 'train_user'+str(args['target'])+".csv"
+        train = pd.read_csv(TRAIN_PATH)
+        y = train["user_id"]
+        y = y.apply(lambda x: 0 if x==args['target'] else 1)
+        X = train.drop("user_id",axis = 1)
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=args['split'],
+                                        test_size=1-args['split'],random_state=10,stratify=y)
+        cat_cols = [cname for cname in X_train.columns if
+                                X_train[cname].dtype == "object"]
+        numerical_cols = [cname for cname in X_train.columns if
+                          X_train[cname].dtype in ['int64', 'float64']]
+        categorical_transformer_oh = Pipeline(steps=[
+            ('onehot', OneHotEncoder(handle_unknown='ignore',sparse=False))
         ])
-    models = {
-        'xgboost':XGBClassifier(objective='binary:logistic',
-                        n_estimators = 150,
-                        eval_metric = 'auc',
-                        max_depth = 3,
-                        random_state = 1,
-                        min_child_weight = 0.5,
-                        scale_pos_weight = 1,
-                        use_label_encoder=False),
-        'gradient_boost':GradientBoostingClassifier(n_estimators=300, learning_rate=0.01,
-        max_depth=6),
-        'tree' : DecisionTreeClassifier(class_weight = 'balanced',criterion = 'entropy'),
-        'forest':RandomForestClassifier(class_weight = 'balanced',criterion= 'entropy'),
-        'ada' : AdaBoostClassifier(base_estimator = DecisionTreeClassifier(class_weight = 'balanced',criterion = 'entropy'))      
-    }
-    model=models[args['model']]
-    pipe = Pipeline(steps=[('preprocessor', preprocessor),
-                        ('standardscaler', StandardScaler(with_mean=False)),
-                                ('model', model)
-                                ])
-    pipe.fit(X_train, y_train)
-
-    preds = pipe.predict(X_valid)
-    tn, fp, fn, tp = confusion_matrix(y_valid,preds).ravel()
-    print('True negatives',tn,'\nFalse positives',fp,'\nFalse negatives',fn,'\nTrue positives',tp)
-    print(classification_report(y_valid,preds))
-    results = cal_threshold(pipe,X_valid,y_valid)
-    
-
-
-    preds = th_predict(X_valid,results[0],results[1])
-    print(classification_report(y_valid,preds))
-    tn, fp, fn, tp = confusion_matrix(y_valid,preds).ravel()
-    print('True negatives',tn,'\nFalse positives',fp,'\nFalse negatives',fn,'\nTrue positives',tp)
-
-    joblib.dump(pipe,'find_user_model')
+        numerical_transformer = SimpleImputer(strategy='mean')
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', numerical_transformer, numerical_cols),
+                ('hot', categorical_transformer_oh, cat_cols)
+            ])
+        models = {
+            'xgboost': XGBClassifier(objective='binary:logistic',
+                            n_estimators = 150,
+                            eval_metric = 'auc',
+                            max_depth = 3,
+                            random_state = 1,
+                            min_child_weight = 0.5,
+                            scale_pos_weight = 1,
+                            use_label_encoder=False),
+            '': GradientBoostingClassifier(n_estimators=300, learning_rate=0.01,
+            max_depth=6),
+            'tree': DecisionTreeClassifier(class_weight = 'balanced', criterion = 'entropy'),
+            'forest': RandomForestClassifier(class_weight = 'balanced', criterion= 'entropy'),
+            'ada': AdaBoostClassifier(base_estimator = DecisionTreeClassifier(
+                class_weight = 'balanced', criterion = 'entropy'))
+        }
+        model=models[args['model']]
+        pipe = Pipeline(steps=[('preprocessor', preprocessor),
+                            ('standardscaler', StandardScaler(with_mean=False)),
+                                    ('model', model)
+                                    ])
+        pipe.fit(X_train, y_train)
+        preds = pipe.predict(X_valid)
+        tn, fp, fn, tp = confusion_matrix(y_valid, preds).ravel()
+        print('True negatives', tn, '\nFalse positives', fp,
+              '\nFalse negatives', fn, '\nTrue positives', tp)
+        print(classification_report(y_valid,preds))
+        results = cal_threshold(pipe,X_valid,y_valid)
+        preds = th_predict(X_valid, results[0], results[1])
+        print(classification_report(y_valid, preds))
+        tn, fp, fn, tp = confusion_matrix(y_valid, preds).ravel()
+        print('True negatives', tn, '\nFalse positives', fp,
+              '\nFalse negatives', fn, '\nTrue positives', tp)
+        joblib.dump(results,'find_user_model.sav')
+    else:
+        results = joblib.load(args['load_model'])
     if args['predict']:
         if args['dataset_path']:
-            pre_path = args['dataset_pred']
+            PRE_PATH = args['dataset_pred']
         else:
-            pre_path = 'to_pred_user'+str(args['target'])
+            PRE_PATH = 'to_pred_user' + str(args['target']) + ".csv"
         print('making prediction on data')
-        to_pred=pd.read_csv(pre_path)
-        predictions = th_predict(to_pred,results[0],results[1])
+        to_pred = pd.read_csv(PRE_PATH)
+        predictions = th_predict(to_pred, results[0], results[1])
         print(predictions)
-        pd.DataFrame(predictions).to_csv('predictions')
-
+        pd.DataFrame(predictions).to_csv('predictions.csv',index = False)
